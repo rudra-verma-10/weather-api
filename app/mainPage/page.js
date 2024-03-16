2958ff44de824387922f3d944798c1d039cec0d1
rudra-verma-10 <rudra.verma@edu.sait.ca>
2024-03-16 17:22:08 -0600

    welcome page
    done work on the welcome page adding all the components and give a structure to the app

---
+++ app/mainPage/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <main className='bg-cover bg-blue-500 bg-gradient-to-t from-blue-500 to-blue-300 h-screen'>
+    <div>
+      
+    </div>
+    </main>
+  )
+}
+
+export default page;
