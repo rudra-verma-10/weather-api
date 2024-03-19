342875cf13475530bea236897ff5306a6ac8dee6
rudra-verma-10 <rudra.verma@edu.sait.ca>
2024-03-19 14:43:38 -0600

    page.js
    making the api key work

--- app/mainPage/page.js
+++ app/mainPage/page.js
@@ -1,17 +1,17 @@
 'use client';
 
-import React, {useState} from 'react'
+import React, {useState, } from 'react'
 import Input from '../components/input';
 // require('dotenv').config();
 
-
+// useEffect(()=> {console.log("key: ", process.env.NEXT_)}, []);
 const page = () => {
   const [location, setLocation] = useState('');
   const [data, setData] = useState({});
 
-  const apiKey = process.env.REACT_APP_API_KEY;
+  const apiKey = process.env.NEXT_PUBLIC_API_KEY;
 
-  const url = `http://api.weatherapi.com/v1/forecast.json?key=80fa953b0f13487f8cd221145241803&q=${location}&days=7&aqi=yes&alerts=yes`;
+  const url = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=7&aqi=yes&alerts=yes`;
 
   const fetchData = async (e) => {
     if (e.key === 'Enter') {
@@ -24,6 +24,7 @@ const page = () => {
       }
       const data = await response.json();
       setData(data);
+      // console.log('data:', data);
       setLocation('');
       
     } catch (error) {
@@ -39,8 +40,18 @@ const page = () => {
     <div className='w-1/4 mt-8'>
       Weather API app
     </div>
-    <div>
-
+    <div className='flex border-2 p-2'>
+      {data.location && (
+        <div className='border-2 h-fit'>
+          <h1>{data.location.name}</h1>
+          <h2>{data.location.region}</h2>
+          <h3>{data.location.country}</h3>
+          <h4>{data.location.localtime}</h4>
+          <h5>{data.current.temp_c}°C</h5>
+          <h6>{data.current.condition.text}</h6>
+          <img src={data.current.condition.icon} alt='weather icon'/>
+        </div>
+      )}
     </div>
     </main>
   )
